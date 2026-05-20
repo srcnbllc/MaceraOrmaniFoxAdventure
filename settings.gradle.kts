54b2d28f3fe50c9968626e09fa7853ca7cd40d95
mucahit2905 <kizildemirmucahit97@gmail.com>
2026-05-20 15:29:15 +0300

    Navigation surumu ve depolari guncellendi

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
     }
 }
